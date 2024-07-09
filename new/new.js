1b401fbc0da5e1d1f9585757869683eccdc200ab
Manas-xt <manas2325k@gmail.com>
2024-07-09 12:57:03 +0530

    drop

--- new/new.js
+++
@@ -1,9 +0,0 @@
-function showsidebar(){
-    const showsidebar = document.querySelector('.sidebar')
-    showsidebar.style.display = 'flex'
-}
-function hidesidebar(){
-        const showsidebar = document.querySelector('.sidebar')
-        showsidebar.style.display = 'none'
-}
-
